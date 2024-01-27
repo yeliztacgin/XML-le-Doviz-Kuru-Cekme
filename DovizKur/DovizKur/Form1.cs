3e685f58db6e58fdc5b3c06774343a3b3fd7e070
yeliztacgin <145569090+yeliztacgin@users.noreply.github.com>
2024-01-27 20:51:12 +0300

    Add files via upload

---
+++ DovizKur/DovizKur/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace DovizKur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(bugun);
+            DateTime tarih = Convert.ToDateTime(xmldoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+            string USD = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/BanknoteSelling").InnerXml;
+
+            label1.Text = string.Format("Tarih {0} USD;{1}", tarih.ToShortDateString(), USD);
+
+
+
+            string EURO = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/BanknoteSelling").InnerXml;
+
+            label2.Text = string.Format("Tarih {0} EURO;{1}", tarih.ToShortDateString(), EURO);
+
+
+
+            string POUND = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/BanknoteSelling").InnerXml;
+
+            label3.Text = string.Format("Tarih {0} POUND;{1}", tarih.ToShortDateString(), POUND);
+
+
+        }
+    }
+}
